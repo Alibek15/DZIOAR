49422eecce6765c37ed12978c4fc978bdd2d616d
Alibek <52044412+Alibek15@users.noreply.github.com>
2020-01-30 00:29:39 +0600

    Add files via upload
    Домашка с презентации

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             ForegroundColor = ConsoleColor.Yellow;
+           
+            int[] MyArray = new int[20] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int Max = MyArray[0];
+            int nul = 0;
+
+            
+
+
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                if (Max < MyArray[i])
+                {
+                    Max = MyArray[i];
+                    nul= i;
+                }
+
+            }
+
+
+            MyArray[nul] = MyArray[0];
+            MyArray[0] = Max;
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                WriteLine(MyArray[i]);
+
+            }
+
+            ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp43/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Cyan;
+            double[] array = new double[18];
+            double[] array2 = new double[18];
+            Random rand = new Random();
+
+            for (int i = 0; i < 8; i++)
+
+            {
+                array[i] = rand.Next(-50, 50);
+
+                WriteLine((array[i]));
+
+            }
+            ResetColor();
+            WriteLine("-------------------------");
+            ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < 8; i++) //i<18
+            {
+                array2[i] = 0.13 * array[i] * array[i] * array[i] - 2.5 * array[i] + 8.0;
+                WriteLine(array2[i]);
+}
+            ReadKey();
+
+        }
+    }
+}
